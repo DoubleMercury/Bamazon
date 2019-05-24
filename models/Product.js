0c0a53fe33f65ba35b5e281fc5892a5b5f246870
Double Mercury <maafakeout@yahoo.com>
2019-05-23 21:22:05 -0400

    populated initial file structures with a sprinkle of code

---
+++ models/Product.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+    var Product = sequelize.define("Product", {
+    product_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {len: [1,160]}
+      },
+    department_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {len: [1]}
+      },
+    price:{
+        type: DataTypes.INTEGER,
+       allowNull: false
+        },
+    stock_quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+      }
+   
+    )};
+    return Post;
+
+   
